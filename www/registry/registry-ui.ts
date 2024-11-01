51177cd569c2f769bdf82a4fec112cee573c92f4
Jake Holman <me@jakeisonline.com>
2024-11-01 22:21:12 +0000

    Revert "declare utils as reg dependency"
    This reverts commit e17f610f86870abfbca695b61a7a7207cc6defc1.

--- www/registry/registry-ui.ts
+++ www/registry/registry-ui.ts
@@ -6,13 +6,11 @@ export const ui: Registry = [
     type: "registry:ui",
     description: "A stepper component",
     files: ["ui/stepper.tsx"],
-    registryDependencies: ["utils"],
   },
   {
     name: "range",
     type: "registry:ui",
     description: "A range component",
     files: ["ui/range.tsx"],
-    registryDependencies: ["utils"],
   },
 ]
